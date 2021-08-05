67a4d86d331b8d571287eb5ea54218809110bf2a
Psaruklily <liliia.psaruk@pickethomes.com>
2021-08-05 17:39:32 +0300

    React hooks: useReduce

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useReducer } from 'react';
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case "SET_TODO": {
+            return action.payload;
+        }
+        case "CHANGE_TODO_STATUS": {
+            return {
+                ...state,
+                completed: !state.completed,
+            };
+        }
+        case "CHANGE_TODO_TITLE": {
+            return {
+                ...state,
+                title: action.payload,
+            };
+        }
+        default: {
+            return state;
+        }
+    }
+};
+
+const initialState = {
+    userId: null,
+    id: null,
+    title: '',
+    completed: false
+}
 
 export default function App() {
 
     const [counter, setCounter] = useState(1);
-    const [todo, setTodo] = useState();
+    const [state, dispatch] = useReducer(reducer, initialState);
 
     useEffect(() => {
         console.log('Called useEffect');
 
-        if([2, 4, 6].some((elem) => elem === counter)) {
-            fetch(`https://jsonplaceholder.typicode.com/todos/${counter}`)
-            .then(response => response.json())
-            .then(json => setTodo(json))
-        }
+        fetch(`https://jsonplaceholder.typicode.com/todos/${counter}`)
+        .then(response => response.json())
+        .then(json => {
+            dispatch({type: 'SET_TODO', payload: json})
+        })
     }, [counter]);
 
     const onClickHandler = () => setCounter((prev) => prev + 1);
+    const onStatusChange = () => dispatch({type: 'CHANGE_TODO_STATUS'});
+    const onTitleChange = () => dispatch({type: 'CHANGE_TODO_TITLE', payload: Math.random()});
 
     return(
         <div>
@@ -24,15 +56,17 @@ export default function App() {
 
             <h1>{counter}</h1>
 
-            {!!todo && (
+            {!!state  && (
                 <>
-                    <h2>{todo.id}</h2>
-                    <h2>{todo.title}</h2>
-                    <h2>{todo.completed.toString()}</h2>
+                    <h2>{state.id}</h2>
+                    <h2>{state.title}</h2>
+                    <h2>{state.completed.toString()}</h2>
                 </>
             )}
             
             <button onClick={onClickHandler}>+</button>
+            <button onClick={onStatusChange}>Change todo status</button>
+            <button onClick={onTitleChange}>Change todo title</button>
         </div>
     )
 }
