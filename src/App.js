1e1c544642ee5b632b4a3c403068de51b7f4725b
Psaruklily <liliia.psaruk@pickethomes.com>
2021-08-06 11:59:05 +0300

    Get faked user from jsonplaceholder after every click by button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React, { Component } from 'react';
+import React, {useState, useEffect} from 'react';
 
-class App extends Component {
+export default function App() {
 
-    render() {
+    const [counter, setCounter] = useState(1);
+    const [user, setUser] = useState();
+    // const [todo, setTodo] = useState();
 
-        return (
-            <div>
-              
-            </div>
-        );
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/todos/${counter}`)
+            .then(response => response.json())
+            .then(json => {
+                console.log(json);
+                setUser(json);
+            })
+    }, [counter]);
+
+    const increaseCounter = () => {
+        setCounter(prev => prev + 1);
     }
-}
 
-export default App;
+    return(
+        <div>
+            <h1>
+                Fake users from jsonplaceholder:
+            </h1>
+            {
+                !!user && (
+                    <>
+                        <h2>{user.id}</h2>
+                        <h2>{user.title}</h2>
+                        <h2>{user.completed.toString()}</h2>
+                    </>
+                )
+            }
+            <button onClick={increaseCounter}>new user</button>
+        </div>
+    )
+}
