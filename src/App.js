f05bd26ff97b504452d769043587ede637581c4f
Psaruklily <liliia.psaruk@pickethomes.com>
2021-08-09 11:08:31 +0300

    React hook useReducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useReducer} from 'react';
+
+const initialState = {
+    completed: false,
+    id: null,
+    title: null
+}
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case "SET_TODO": {
+            return action.payload;
+        }
+        case "CHANGE_TODO_STATUS": {
+            return {
+                ...state,
+                completed: !state.completed
+            }
+        }
+        case "CHANGE_TODO_TITLE": {
+            return {
+                ...state,
+                title: action.payload
+            }
+        }
+        default: {
+            return state;
+        }
+    }
+}
 
 export default function App() {
 
     const [counter, setCounter] = useState(1);
     const [user, setUser] = useState();
+    const [state, dispatch] = useReducer(reducer, initialState);
     // const [todo, setTodo] = useState();
 
     useEffect(() => {
@@ -11,13 +41,18 @@ export default function App() {
             .then(response => response.json())
             .then(json => {
                 console.log(json);
-                setUser(json);
-            })
+                // setUser(json);
+                dispatch({type: 'SET_TODO', payload: json});
+            });
+        return () => { console.log('unmount'); }
     }, [counter]);
 
     const increaseCounter = () => {
         setCounter(prev => prev + 1);
     }
+
+    const onStatusChange = () => dispatch({type: 'CHANGE_TODO_STATUS'});
+     const onTitleChange = () => dispatch({type: 'CHANGE_TODO_TITLE', payload: Math.random()});
 
     return(
         <div>
@@ -25,15 +60,17 @@ export default function App() {
                 Fake users from jsonplaceholder:
             </h1>
             {
-                !!user && (
+                !!state && (
                     <>
-                        <h2>{user.id}</h2>
-                        <h2>{user.title}</h2>
-                        <h2>{user.completed.toString()}</h2>
+                        <h2>{state.id}</h2>
+                        <h2>{state.title}</h2>
+                        <h2>{state.completed.toString()}</h2>
                     </>
                 )
             }
             <button onClick={increaseCounter}>new user</button>
+            <button onClick={onStatusChange}>Change todo status</button>
+            <button onClick={onTitleChange}>Change todo title</button>
         </div>
     )
 }
