02586e343c6fa182a7c654497b1df447ee416edc
Psaruklily <liliia.psaruk@pickethomes.com>
2021-08-09 13:37:13 +0300

    React hook useEffect: return function (ComponentWillUnMount)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+const Test = () => {
+
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        console.log('from main function');
+    
+        return () => {
+            console.log('from cleanup function'); 
+        }
+       
+    }, [counter]);
+
+    return(
+        <h2 onClick={() => setCounter(prev => prev + 1) }>From test component. Counter: {counter}</h2>
+    )
+}
 
 export default function App() {
+
+    const [isVisible, setIsVisible] = useState(false);
+
     return(
         <div>
-
+            <button onClick={() => setIsVisible(!isVisible)}>onToggle</button>
+            {isVisible && <Test />}
         </div>
     )
 }
