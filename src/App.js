ca6cc4947706bf8c9fd03d6782d3f266505ef3c3
Psaruklily <liliia.psaruk@pickethomes.com>
2021-08-11 11:27:13 +0300

    Creating counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {INC_COUNTER, DEC_COUNTER, RESET_COUNTER} from './redux/action-types';
 
 export default function App() {
+
+    const counter = useSelector(({counter}) => counter);
+    const dispatch = useDispatch();
+
+    const handleInc = () => dispatch({type: INC_COUNTER});
+    const handleDec = () => dispatch({type: DEC_COUNTER});
+    const handleReset = () => dispatch({type: RESET_COUNTER});
+
     return(
         <div>
-
+            <h2>Counter: {counter}</h2>
+            <button onClick={handleInc}>inc</button>
+            <button onClick={handleDec}>dec</button>
+            <button onClick={handleReset}>reset</button>
         </div>
     )
 }
